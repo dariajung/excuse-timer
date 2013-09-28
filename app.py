e795a3ea44c4b764c5dad4bf7d434a8a9cf57e29
Daria Jung <djj2115cu@gmail.com>
2013-09-28 16:59:22 -0400

    add index template

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ from flask import Flask
 from credentials import account_id, token_id
 from twilio.rest import TwilioRestClient
 from flask.ext.pymongo import PyMongo
+from flask import render_template
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 mongo = PyMongo(app)
 
 @app.route("/")
-def hello():
-        return "Hello World!"
+def index():
+        return render_template('index.html')
 
 if __name__ == "__main__":
 	app.run()
