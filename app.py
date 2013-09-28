078891c8a00bf291f4bb82d347985918e8ddeb7c
Matthew Bunday <zenceph@gmail.com>
2013-09-28 17:23:41 -0400

    Parse message with parsedatetime

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ from credentials import account_id, token_id
 from twilio.rest import TwilioRestClient
 from flask.ext.pymongo import PyMongo
 from flask import render_template
+import parsedatetime as pdt
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 mongo = PyMongo(app)
+c = pdt.Constants()
+p = pdt.Calendar(c)
 
 @app.route("/")
 def index():
@@ -17,6 +20,8 @@ def index():
 def receive_msg():
     number = request.args.get('From', None)
     message = request.args.get('Body', None)
+    datetime = p.parse("message")
+    
     return "Hello"
 
 if __name__ == "__main__":
