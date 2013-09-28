e214a7f5c61c41fd1449aea18e13888176ad66f3
Daria Jung <djj2115cu@gmail.com>
2013-09-28 16:34:21 -0400

    import credentials

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from credentials import account_id, token_id
+from twilio.rest import TwilioRestClient
+from flask.ext.pymongo import PyMongo
+
 app = Flask(__name__)
 
 @app.route("/")
