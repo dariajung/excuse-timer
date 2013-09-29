1258bfdbd2e54215dff138ef7b04f8014706fe53
Matthew Bunday <zenceph@gmail.com>
2013-09-29 02:37:16 +0000

    Make it not actually a cron thing, but a while loop

--- call-cron.py
+++ call-cron.py
@@ -2,13 +2,16 @@ from pymongo import MongoClient
 from datetime import datetime
 from twilio.rest import TwilioRestClient
 from credentials import account_id, token_id
+from time import sleep
 
 client = MongoClient()
 messages = client['excuse-timer'].messages
 client = TwilioRestClient(account_id, token_id)
 
-for message in messages.find({"time":{"$lt":datetime.now()}}):
-    call = client.calls.create(to=message['from'], from_='+14242299993', url='http://zencephalon.com:5000/twilio')
-    messages.remove(message)
-    print call.sid
-    print message
+while True:
+    for message in messages.find({"time":{"$lt":datetime.now()}}):
+        call = client.calls.create(to=message['from'], from_='+14242299993', url='http://zencephalon.com:5000/twilio')
+        messages.remove(message)
+        print call.sid
+        print message
+    sleep(60)
