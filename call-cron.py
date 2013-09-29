343d81b0a79dc66b7a27074dec3136c0dc8ab16e
Matthew Bunday <zenceph@gmail.com>
2013-09-29 21:01:18 +0000

    Fix bug

--- call-cron.py
+++ call-cron.py
@@ -12,10 +12,10 @@ while True:
     for message in messages.find({"time":{"$lt":datetime.now()}}):
         try:
             call = client.calls.create(to=message['from'], from_='+14242299993', url='http://zencephalon.com:5000/twilio')
+            print call.sid
+            print message
         except:
             print "an error occurred"
         finally:
             messages.remove(message)
-        print call.sid
-        print message
     sleep(60)
